2fc71e0f29abfdb70648ee7f31ed4fd392c33efc
Mike <80423783+MikeTheHash@users.noreply.github.com>
2022-11-19 16:22:24 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install pip3 -y
+
+pip install requests
+pip install argparse
+pip install colorama
+pip install time
+pip install os
+
